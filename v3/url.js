620cbb49988ad2386a2292d91143bb4488910d34
krb999 <68408633+krb999@users.noreply.github.com>
2020-07-17 10:07:24 +0800

    Add files via upload

---
+++ v3/url.js
@@ -0,0 +1,22 @@
+function getRandomArrayElements(arr, count) {
+    var shuffled = arr.slice(0), i = arr.length, min = i - count, temp, index;
+    while (i-- > min) {
+        index = Math.floor((i + 1) * Math.random());
+        temp = shuffled[index];
+        shuffled[index] = shuffled[i];
+        shuffled[i] = temp;
+    }
+    return shuffled.slice(min);
+}
+var items = [
+'krb9.com',
+'krb99.com',
+'krb999.com'
+];
+var item=getRandomArrayElements(items, 3);
+var djgk = ['点击观看'];
+document.write('<a href="http://' + item[0] + '" target="_blank" class="list-group-item"><p class="list-group-item-text">' + item[0] + '<em class="pull-right">' + djgk[0] +'</em></p></a>');
+document.write('<a href="http://' + item[1] + '" target="_blank" class="list-group-item"><p class="list-group-item-text">' + item[1] + '<em class="pull-right">' + djgk[0] +'</em></p></a>');
+document.write('<a href="http://' + item[2] + '" target="_blank" class="list-group-item"><p class="list-group-item-text">' + item[2] + '<em class="pull-right">' + djgk[0] +'</em></p></a>');
+
+
